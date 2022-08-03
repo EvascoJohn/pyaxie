157addf842fff9bf8a068155926b75e608503764
EvascoJohn <antugaevasco@gmail.com>
2022-08-03 11:20:09 +0800

    improved types

--- axie_graphql_query.py
+++ axie_graphql_query.py
@@ -2,26 +2,29 @@
 # recursive function.
 # searches the hashmap, to know if the given key is available.
 def hashmap_key_searcher(dictionary, target):
+    try:
+        """ Takes a dictionary/hashmap and searches a key. """
 
-    """ Takes a dictionary/hashmap and searches a key. """
+        # checks each item on the dictionary
+        for key, value in dictionary.items():
 
-    # checks each item on the dictionary
-    for key, value in dictionary.items():
+            # checks if the key is equals to the target.
+            if key == target:
 
-        # checks if the key is equals to the target.
-        if key == target:
+                # returns value
+                return value
 
-            # returns value
-            return value
+            # checks if the value is another dictionary.
+            if type(value).__name__ == dict.__name__:
 
-        # checks if the value is another dictionary.
-        if type(value).__name__ == dict.__name__:
+                # runs the function again.
+                return hashmap_key_searcher(value, target)
+        
+        # ends
+        return
+    except:
 
-            # runs the function again.
-            return hashmap_key_searcher(value, target)
-    
-    # ends
-    return
+        return None
 
 
 class AxieQuery:
@@ -32,7 +35,8 @@ class AxieQuery:
 
     def __init__(self, response_json=None):
         self.response_json = response_json
-    
+        if(type(response_json).__name__ != dict.__name__):
+            self.response_json = None
 
     def get_data(self) -> dict:
         return hashmap_key_searcher(self.response_json, "data")
--- axie_model.py
+++ axie_model.py
@@ -7,15 +7,18 @@ class Axie:
     """
 
     def __init__(self, dictionary=None):
-        self.dictionary = dictionary
-        self.id = self.dictionary['id']
-        self.name = self.dictionary['name']
-        self.stage = self.dictionary['stage']
-        self.axie_class = self.dictionary['class']
-        self.breed_count = self.dictionary['breedCount']
-        self.image = self.dictionary['image']
-        self.title = self.dictionary['title']
-        self.auction = self.dictionary['auction']
-        self.usd = self.dictionary['auction']['currentPriceUSD']
-        self.battleinfo = self.dictionary['battleInfo']
-        self.parts = self.dictionary["parts"]
+        if type(dictionary).__name__ != dict.__name__:
+            self.dictionary = None
+        else:
+            self.dictionary = dictionary
+            self.id = self.dictionary['id']
+            self.name = self.dictionary['name']
+            self.stage = self.dictionary['stage']
+            self.axie_class = self.dictionary['class']
+            self.breed_count = self.dictionary['breedCount']
+            self.image = self.dictionary['image']
+            self.title = self.dictionary['title']
+            self.auction = self.dictionary['auction']
+            self.usd = self.dictionary['auction']['currentPriceUSD']
+            self.battleinfo = self.dictionary['battleInfo']
+            self.parts = self.dictionary["parts"]
